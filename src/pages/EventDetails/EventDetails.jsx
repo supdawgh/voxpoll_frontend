abde71ee67f2137ec706c1b37fe9ff15047c4b8f
Adarsh Das <adarsh.191605@ncit.edu.np>
2024-07-02 15:00:09 +0545

    added winner

--- src/pages/EventDetails/EventDetails.jsx
+++ src/pages/EventDetails/EventDetails.jsx
@@ -7,6 +7,22 @@ import useSWR from "swr";
 import { format } from "date-fns";
 import Web from "../Web/Web";
 
+function getCandidateWithHighestVotes(candidates) {
+  if (candidates.length === 0) {
+    return null; // Return null if the candidates array is empty
+  }
+
+  let highestVoteCandidate = candidates[0];
+
+  for (let i = 1; i < candidates.length; i++) {
+    if (candidates[i].voteCount > highestVoteCandidate.voteCount) {
+      highestVoteCandidate = candidates[i];
+    }
+  }
+
+  return highestVoteCandidate;
+}
+
 const fetcher = (url) => fetch(url).then((r) => r.json());
 
 const EventDetails = ({ setShowLogin }) => {
@@ -19,7 +35,18 @@ const EventDetails = ({ setShowLogin }) => {
     isLoading,
   } = useSWR(`${API_BASE_URl}/events/${id}`, fetcher);
 
+  let candidates = [];
+
+  if (event) {
+    candidates = event.candidates;
+  }
+
+  const winner = getCandidateWithHighestVotes(candidates);
   const today = new Date();
+
+  if (event) {
+    candidates = event;
+  }
 
   if (error) return <div>failed to load</div>;
   if (isLoading) return <div>loading...</div>;
@@ -27,11 +54,38 @@ const EventDetails = ({ setShowLogin }) => {
   return (
     <div className="event">
       <div className="event-name">
-        <div className="event-name-list">
-          <h2>{event.eventName}</h2>
-          <p>{event.eventDescription}</p>
-          <p>Candidates :{event.candidates.length}</p>
-          <p>Ends on :{format(event.eventEndDate, "PPP")}</p>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <div className="event-name-list">
+            <h2>{event.eventName}</h2>
+            <p>{event.eventDescription}</p>
+            <p>Candidates :{event.candidates.length}</p>
+            <p>Ends on :{format(event.eventEndDate, "PPP")}</p>
+          </div>
+          {new Date(event.eventEndDate) < today && (
+            <div>
+              <div className="name-item">
+                <div className="name-item-img-container">
+                  <img
+                    className="name-item-image"
+                    src={winner.photo}
+                    alt={winner.name}
+                  />
+                  <p style={{ textAlign: "center", fontWeight: "bold" }}>
+                    Winnerrr !!
+                  </p>
+                </div>
+                <div className="name-item-info">
+                  <h3 style={{ textAlign: "center" }}>{winner.name}</h3>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
         <br />
         <hr />
